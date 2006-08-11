bde5c5b07b59bf2e2a8d0b0b62cc30d66774902d
Yuval Kogman <nothingmuch@woobling.org>
2006-08-11 17:17:15 +0000

    Documentation improvements

--- lib/Cache/Cascade.pm
+++ lib/Cache/Cascade.pm
@@ -123,7 +123,7 @@ semantics.
 
 	Cache::Cascade->new(
 		caches => [
-			Cache::Memory->new(...),
+			Cache::Bounded->new(...),
 			Cache::FastMmap->new(...),
 			Cache::Memcached->new(...),
 		],
@@ -136,22 +136,22 @@ semantics.
 In a multiprocess, and especially a multiserver application caching is a very
 effective means of improving results.
 
-The tradeoff with improving the scale of the caching is in added complexity.
+The tradeoff of increasing the scale of the caching is in added complexity.
 For example, caching in a FastMmap based storage is much slower than using a
 memory based cache, because pages must be locked to ensure that no corruption
 will happen. Likewise Memcached is even more overhead than FastMmap because it
 is network bound, and uses blocking IO (on the client side).
 
-This module attempts to make a transparent cascade of caches based on several
+This module attempts to make a transparent cascade of caches using several
 backends.
 
 The idea is to search from the cheapest backend to the most expensive, and
 depending on the options also cache results in the chepear backends.
 
-The benefita of using a cascade are that if the chance of a hit is much higher
+The benefits of using a cascade are that if the chance of a hit is much higher
 in a slow cache, but checking a cheap cache is negligiable in comparison, we
-may alreayd have the result we want in the cheap cache. Configure your
-expriation policy so that there is approximately an order of magnitude better
+may already have the result we want in the cheap cache. Configure your
+expiration policy so that there is approximately an order of magnitude better
 probability of cache hits (bigger cache) for each level of the cascade.
 
 =item FIELDS
